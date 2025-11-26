7287434d38071d5a185312d51956d04fcb3b6cb8
Roberto Cirera Procópio Jr <35637328+rocpjunior@users.noreply.github.com>
2025-11-26 09:05:50 -0300

    more small changes

--- app/src/main/java/com/example/fragmentos/VisualizandoCards.kt
+++ app/src/main/java/com/example/fragmentos/VisualizandoCards.kt
@@ -21,16 +21,16 @@ class VisualizandoCards : AppCompatActivity() {
             arrayOf("https://www.notebookcheck.info/fileadmin/Notebooks/News/_nc5/Hollow-Knight-Silksong-6-million-copies-sold.jpg", "Jogos", "Descriçao")
         )
         for(elemento in listaDeProdutos){
-            val Card = inflamar.inflate(R.layout.card, controladorDeCards, false)
-            val img = Card.findViewById<ImageView>(R.id.imgCard)
-            val titulo = Card.findViewById<TextView>(R.id.txtNome)
-            val descricao = Card.findViewById<TextView>(R.id.txtDescricao)
+            val card = inflamar.inflate(R.layout.card, controladorDeCards, false)
+            val imagem = card.findViewById<ImageView>(R.id.imgCard)
+            val titulo = card.findViewById<TextView>(R.id.txtNome)
+            val descricao = card.findViewById<TextView>(R.id.txtDescricao)
             Glide.with(this)
                 .load(elemento[0])
-                .into(img)
+                .into(imagem)
             titulo.text = elemento[1]
             descricao.text = elemento[2]
-            controladorDeCards.addView(Card)
+            controladorDeCards.addView(card)
         }
     }
 }
